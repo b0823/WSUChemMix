9ff8495f115ab14e0a9bffa2b99a2b85f3598faf
brad reynolds <reynolds.0345@gmail.com>
2016-03-25 23:05:26 -0400

    dsfgsgdfsgdf
    fgsdfg

--- Math/Math.js
+++ Math/Math.js
@@ -168,6 +168,10 @@ function calcMolecularWeight(X,classParams){
 module.exports.mainFunction = function(theirParams, classParams, classInputRanges){
 	//Temp, Concentration, Feed Rate, Pressure, Time. Could add name tags like functionParams if it helps.
 
+    console.log(theirParams)
+    console.log(classParams)
+    console.log(classInputRanges)
+
 	var X = createData(theirParams,classParams,classInputRanges);
 
 	var moleWeight = calcMolecularWeight(X,classParams);
